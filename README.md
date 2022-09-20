34ea4a7f181483829e2a18a265d5566daf6d626d
Pig Frown <pigfrown@protonmail.com>
2022-09-20 16:38:01 +0100

    feat: adds script to check relay registration

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# check_mevboost_registration
+# Check MEVBoost Registration
+
 Script to check if a public key is registered with a list of MEVBoost relays
+
+# Requirements
+
+* Bash version 4 or great
+* curl
+* jq
+* an emoji capable terminal 🤷
+
+# Usage
+
+./check_validators.sh YOURPUBLICKEY
+
+
+# Example
+
+(I picked a random validator key from beaconcha.in for this example)
+
+```
+$ ./check_validators.sh 0x8000558843d93fd74ad009d1f120e657c09fea88c44d1d739dccbafdca9e6f5df6d2649825ac7db5f2078aade4bf03cb
+
+Checking if public key 0x8000558843d93fd74ad009d1f120e657c09fea88c44d1d739dccbafdca9e6f5df6d2649825ac7db5f2078aade4bf03cb is registered...
+BloXroute_max_profit ✅
+BloXroute_ethical ✅
+Blocknative ✅
+Flashbots ✅
+Eden ❌
+BloXroute_regulated ✅
+```
+
+# Relays
+
+The following relays are checked:
+
+* BloXroute max profit,  https://bloxroute.max-profit.blxrbdn.com
+* BloXroute ethical, https://bloxroute.ethical.blxrbdn.com
+* BloXroute regulated, https://bloxroute.regulated.blxrbdn.com
+* Blocknative, https://builder-relay-mainnet.blocknative.com
+* Eden, https://relay.edennetwork.io
+* Flashbots, https://boost-relay.flashbots.net
+
+Manifold's relay is not included because they do not implement the validator_registration endpoint. 
+
---
+++ check_validators.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+
+if [ -z ${1+x} ] ; then
+	echo "Usage: ./check_validators PUBLICKEY"
+	exit 1
+fi
+
+validator_key_to_check=$1
+# Check we have required programs
+if ! command -v curl &> /dev/null ; then
+	echo curl command is required.. bye bye
+	exit 2
+fi
+
+if ! command -v jq &> /dev/null ; then
+	echo jq command is required.. bye bye
+	exit 3
+fi
+
+url_path="relay/v1/data/validator_registration?pubkey="
+
+# map where key = relay name, value = relay url
+declare -A relays
+
+relays[BloXroute_max_profit]="https://bloxroute.max-profit.blxrbdn.com"
+relays[BloXroute_ethical]="https://bloxroute.ethical.blxrbdn.com"
+relays[BloXroute_regulated]="https://bloxroute.regulated.blxrbdn.com"
+relays[Blocknative]="https://builder-relay-mainnet.blocknative.com"
+relays[Eden]="https://relay.edennetwork.io"
+relays[Flashbots]="https://boost-relay.flashbots.net"
+
+
+# Manifold doesn't follow the spec.. leaving this out for now
+#relays[Manifold]="https://mainnet-relay.securerpc.com"
+
+
+
+echo Checking if public key $validator_key_to_check is registered...
+
+for key in "${!relays[@]}" ; do
+	check_url="${relays[$key]}/${url_path}$validator_key_to_check"
+	# Some relays redirect, follow the redirection
+	if [ "$(curl -Ls $check_url | jq .code)"  = null ] ; then
+		echo "$key ✅"
+	else
+		echo "$key ❌"
+	fi
+done
+
+
+
